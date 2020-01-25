4810ec8776af0d87295382e1f60fefaaf33c0160
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-01-25 21:16:01 +0800

    测试 Excel CSV 数据读取

---
+++ src/test/java/cn/com/xuxiaowei/easyexcel/read/ReadCsvTests.java
@@ -0,0 +1,34 @@
+package cn.com.xuxiaowei.easyexcel.read;
+
+import cn.com.xuxiaowei.entity.User;
+import com.alibaba.excel.EasyExcel;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 测试 Excel CSV 数据读取
+ * <p>
+ * 暂不能读取
+ */
+@Slf4j
+@SpringBootTest
+class ReadCsvTests {
+
+    /**
+     * 读取本地项目文件夹路径（Spring Boot）
+     */
+    private String projectPath = System.getProperty("user.dir");
+
+    /**
+     * <p>
+     * 使用 easyexcel 时，实体类不可使用<code>@Accessors(chain = true)</code>
+     */
+    @Test
+    void simpleRead() {
+        String simpleRead = projectPath + "/easyexcel/read/simpleRead.csv";
+        log.debug("读取路径：{}", simpleRead);
+        EasyExcel.read(simpleRead, User.class, new UserDataListener()).sheet().doRead();
+    }
+
+}
