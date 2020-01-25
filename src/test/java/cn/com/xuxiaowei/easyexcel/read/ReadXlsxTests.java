91d13cf9ea4a41f734c8cfc6f8256fd1ba2545cf
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-01-25 21:29:40 +0800

    读取并储存

--- src/test/java/cn/com/xuxiaowei/easyexcel/read/ReadXlsxTests.java
+++ src/test/java/cn/com/xuxiaowei/easyexcel/read/ReadXlsxTests.java
@@ -1,9 +1,11 @@
 package cn.com.xuxiaowei.easyexcel.read;
 
 import cn.com.xuxiaowei.entity.User;
+import cn.com.xuxiaowei.service.IUserService;
 import com.alibaba.excel.EasyExcel;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -14,6 +16,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Slf4j
 @SpringBootTest
 class ReadXlsxTests {
+
+    @Autowired
+    private IUserService iUserService;
 
     /**
      * 读取本地项目文件夹路径（Spring Boot）
@@ -30,6 +35,18 @@ class ReadXlsxTests {
         String simpleRead = projectPath + "/easyexcel/read/simpleRead.xlsx";
         log.debug("读取路径：{}", simpleRead);
         EasyExcel.read(simpleRead, User.class, new UserDataListener()).sheet().doRead();
+    }
+
+    /**
+     * 读取并储存
+     * <p>
+     * 使用 easyexcel 时，实体类不可使用<code>@Accessors(chain = true)</code>
+     */
+    @Test
+    void readAndSave() {
+        String simpleRead = projectPath + "/easyexcel/read/simpleRead.xlsx";
+        log.debug("读取路径：{}", simpleRead);
+        EasyExcel.read(simpleRead, User.class, new UserDataListener(iUserService)).sheet().doRead();
     }
 
     /**
